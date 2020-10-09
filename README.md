a7b29b78f02d18c507d377329663807ddd57c737
Caradr42 <carlosAdr2gv@gmail.com>
2020-10-09 16:59:32 -0500

    initial commit for the node-express_js proyect

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Pagina-WISE
+======
+
+Ejecutar el servidor:
+```
+npm run server
+```
+o
+
+```
+node server
+```
+
+
+Ejecutar el servidor en modo development:
+```
+npm run dev
+```
+(se actualiza en tiempo real usando el paquete nodemon)
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//endpoints
+app.use(express.static(path.join(__dirname, 'public')) );
+app.get('/', function(req, res){
+    //res.send('<h1> Hello world!!!</h1>')
+    res.sendFile(path.join(__dirname, 'public', 'index.html') );
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT} on dir ${__dirname}`) );
