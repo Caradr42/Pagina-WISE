fac2c64bd15f5866c3b80f87500975d2d791c5c8
Caradr42 <carlosAdr2gv@gmail.com>
2020-11-12 21:04:26 -0600

    added severside user creation and verification, for restricted acces of route

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const router = express.Router();
-//const mongoose = require('mongoose');
-//const hbs = require('hbs')
-const port = process.env.PORT || 5000;
-const { mongoose } = require('../../BackEnd-API/database/database.connection');
+// const router = express.Router();
+const router = require('express-promise-router')();
 
-//======
+//onst port = process.env.PORT || 5000;
+const { mongoose } = require('../../BackEnd-API/database/database.connection');
+const isLoggedIn = require('../../BackEnd-API/server/userVerification');
+
+//======CONTROLLERS
 const DatosHomeCtrl = require("../../BackEnd-API/server/controllers/datosHomeCtrl");
 const SalonDeLaFamaCtrl = require("../../BackEnd-API/server/controllers/salon_de_la_famaCtrl");
 const PublicacionesCtrl = require("../../BackEnd-API/server/controllers/datosPublicacionesCtrl");
 const PatrocionadoresCtrl = require("../../BackEnd-API/server/controllers/patrocinadoresCtrl");
+const UsersCtrl = require("../../BackEnd-API/server/controllers/usersCtrl");
 //======
 
 function cloneToObjArr(doc) {
@@ -31,6 +33,26 @@ function cloneToObjArr(doc) {
 //             res.render('cv', {cvFields: copy});
 //         });
 // });
+
+//authentification routes
+
+// router.route('/signin')
+//     .post(UsersCtrl.signInOrCreateNew); 
+
+router.get('/failed', (req, res) => {
+    res.send("you failed to log in");
+});
+
+router.get('/good', isLoggedIn, (req, res) => {   
+    res.send(`welcome: ${req.user.name}`);
+});
+
+router.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+});
+
 
 //routes
 router.get('/', (req, res, next) => {
--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,22 +1,23 @@
-
 const path = require('path');
-const routes = require('./routes/index')
 const PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-//const favicon = require('serve-favicon');
+const morgan = require('morgan');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
-
+require('./passport-setup');
 const app = express();
 
-//view engine setup
+//===== view engine setup ===== 
 var layoutsPath = path.join(__dirname, path.join('views', 'layouts'));
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: layoutsPath}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//===== handlebars function example =====
 // var hbsC = hbs.create({});
 // hbsC.handlebars.registerHelper('setColor', function (index) {
 //     if (parseInt(index) % 2 === 0){
@@ -25,15 +26,33 @@ app.set('view engine', 'hbs');
 //     return '#aaa';
 // });
 
-//other
+/// ===== Middlewares =====
+app.use(morgan('dev')); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+    name: 'admin-session',
+    keys: ['key1', 'key2'] 
+}));
 
-//app.use(favicon(path.join(__dirname,'public','favicon.ico.png')));
-//app.use('/favicon.ico', express.static('public/favicon.ico.png'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+///===== OAuth passport request authentication ======
+app.get('/google',  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }
+    ),
+    function(req, res) {
+        // Successful authentication, redirect to good.
+        res.redirect('/good');
+    }
+);
+
 
 //routing
-app.use('/', routes);
+app.use('/', require('./routes/index'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT} on dir ${__dirname}`) );
+
+app.listen(PORT, () => console.log(`\nServer started on port ${PORT} on dir ${__dirname}`) );
